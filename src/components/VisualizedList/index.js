800f228b5619978bf74b6d29d9f1182a120e62d4
Juan Garcia Ramos <juancho.garcia123@gmail.com>
2024-02-18 16:53:38 +0200

    Format table

--- src/components/VisualizedList/index.js
+++ src/components/VisualizedList/index.js
@@ -1,7 +1,26 @@
 import styles from './index.module.css'
 
-export default function VisualizedList({items}) {
-	return <div>
-		{items.map((item) => <div key={item.id} className={styles.list}>{item.id}</div>)}
-	</div>
+export default function VisualizedList({ items }) {
+	return (
+		<div className={styles.container}>
+			<table>
+				<thead className={styles.header}>
+					<tr>
+						<th>Name</th>
+						<th>Description</th>
+						<th>Price</th>
+					</tr>
+				</thead>
+				<tbody>
+					{items.map(({id, itemName, itemDescription, itemPrice}) => (
+						<tr key={id} className={styles.row}>
+							<td>{itemName}</td>
+							<td>{itemDescription}</td>
+							<td>{itemPrice}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	)
 }
